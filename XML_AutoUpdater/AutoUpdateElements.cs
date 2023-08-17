0b22f99827fe30f65624e8bf710660144512886d
ACryInShame <ciller7765@yahoo.com>
2023-08-17 16:35:57 -0400

    Add files via upload
    updated to update all files in the Data folder

--- XML_AutoUpdater/AutoUpdateElements.cs
+++ XML_AutoUpdater/AutoUpdateElements.cs
@@ -1,119 +1,3 @@
-//using System;
-//using System.IO;
-//using System.Linq;
-//using System.Xml;
-
-////class Program
-////{
-////	static void Main(string[] args)
-////	{
-////		
-
-////        // Iterate through the target XML files and update matching lines
-////        foreach (string targetFile in targetXmlFiles)
-////		{
-
-////			// Load the target XML document
-////			targetDoc.Load(targetFile);
-
-////            // Iterate through the elements in the source XML
-////            foreach (XmlNode sourceNode in sourceDoc.DocumentElement.ChildNodes)
-////            {
-
-////                // Skip comment nodes
-////                if (sourceNode.NodeType == XmlNodeType.Comment)
-////                {
-////                    continue;
-////                }
-
-////                XmlElement sourceElem = (XmlElement)sourceNode;
-
-////                PrintOut($"{sourceElem.Name} Found");
-
-////                // Find the corresponding element in the target XML
-////                XmlNode targetNode = targetDoc.SelectSingleNode(sourceElem.Name);
-////                if (targetNode != null)
-////                {
-////                    // Update the text content of the matching element in the target XML
-////                    targetNode.InnerText = sourceElem.InnerText;
-////                }
-////                if (sourceElem.Name == "Music")
-////                    PrintOut("Music Found");
-////            }
-
-////			// Save the updated target XML back to the file
-////			targetDoc.Save(targetFile);
-////		}
-////	}
-
-////    static void PrintOut(string STR)
-////    {
-////        Console.WriteLine(STR);
-////        Console.ReadKey();
-
-////        //Console.WriteLine($"Checking Child Node {sourceElem.Name}");
-////        //Console.ReadKey();
-////    }
-////}
-
-//class Program
-////{
-////    static void Main(string[] args)
-////    {
-////        // Specify the paths to the source and target XML files
-////        string sourceXmlFile = "UpdateElements.xml";  // XML file A
-////        string targetXmlFile = "Data\\World\\Factions\\Eldar.xml";  // XML file B
-
-////        // Load the source XML document
-////        XmlDocument sourceDoc = new XmlDocument();
-////        sourceDoc.Load(sourceXmlFile);
-
-////        foreach (XmlNode node in sourceDoc.DocumentElement.ChildNodes)
-////        {
-////            Console.WriteLine("Node Name: " + node.Name);
-////        }
-
-
-////        // Load the target XML document
-////        XmlDocument targetDoc = new XmlDocument();
-////        targetDoc.Load(targetXmlFile);
-
-////        // Find the first child element in the source XML
-////        XmlElement sourceFirstElement = sourceDoc.DocumentElement.ChildNodes.OfType<XmlElement>() as XmlElement;
-////        if (sourceFirstElement != null)
-////        {
-////            PrintOut($"{sourceFirstElement.Name}");
-////        }
-
-////        // Iterate through the elements in the source XML
-////        foreach (XmlElement sourceElem in sourceDoc.DocumentElement.ChildNodes.OfType<XmlElement>())
-////        {
-////            PrintOut($"{sourceElem.Name}");
-
-////            // Find the corresponding element in the target XML
-////            XmlNode targetNode = targetDoc.SelectSingleNode(sourceElem.Name);
-////            if (targetNode != null)
-////            {
-////                // Replace the content of the matching element in the target XML
-////                targetNode.InnerXml = sourceElem.InnerXml;
-////            }
-////        }
-
-////        // Save the updated target XML back to the file
-////        targetDoc.Save(targetXmlFile);
-
-////        Console.WriteLine("Element content replaced successfully.");
-////    }
-//    static void PrintOut(string STR)
-//    {
-//        Console.WriteLine(STR);
-//        Console.ReadKey();
-
-//    //Console.WriteLine($"Checking Child Node {sourceElem.Name}");
-//    //Console.ReadKey();
-//}
-//}
-
 using System;
 using System.IO;
 using System.Xml;
@@ -125,31 +9,29 @@ class AutoUpdateXmlElements
         bool DebugMode = true;
         // Specify the paths to the source and target XML files
         string sourceXmlFile = "UpdateElements.xml";
-        string targetXmlFile = "Eldar.xml";
+        string targetDirectory = "Data";
         if (DebugMode)
         {
             PrintOut(sourceXmlFile);
-            PrintOut(targetXmlFile);
+            PrintOut(targetDirectory);
         }
 
         //Load the source XML documents
         XmlDocument sourceDoc = new XmlDocument();
         sourceDoc.Load(sourceXmlFile);
-        XmlDocument targetDoc = new XmlDocument();
-        targetDoc.Load(targetXmlFile);
 
-        if (DebugMode)
-        {
-            PrintOut(sourceDoc.InnerXml);
-            PrintOut(targetDoc.InnerXml);
-        }
+        //Set-up target Document. These will load as we iterate through the folders
+        XmlDocument targetDoc = new XmlDocument();
+
+        // Get a list of XML files in the target directory
+        string[] targetXmlFiles = Directory.GetFiles(targetDirectory, "*.xml");
 
         //Iterate through the target XML files and update matching lines
-      //  foreach (string targetFile in targetXmlFiles)
-        //{
+        foreach (string targetFile in targetXmlFiles)
+        {
 
             // Load the target XML document
-            //targetDoc.Load(targetFile);
+            targetDoc.Load(targetFile);
 
             // Iterate through the elements in the source XML
             foreach (XmlNode sourceNode in sourceDoc.DocumentElement.ChildNodes)
@@ -173,15 +55,15 @@ class AutoUpdateXmlElements
                 {
                     if (DebugMode){PrintOut($"Current Target Node: {targetNode.Name}");}
                     // Update the text content of the matching element in the target XML
-                    targetNode.InnerText = sourceElem.InnerText;
+                    targetNode.InnerXml = sourceElem.InnerXml;
                 }
             }
 
             // Save the updated target XML back to the file
-            targetDoc.Save(targetXmlFile);
-     //   }
+            targetDoc.Save(targetFile);
+        }
 
-        PrintOut("End");
+        PrintOut("Finished");
     }
     static void PrintOut(string STR)
     {
